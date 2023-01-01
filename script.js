c15a73b24d437049b60bcd8888310db8cab62582
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-02 02:05:47 +0330

    Implementation of full screen

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let controls = playerArea.querySelector('.myplayer__controls');
 let play = controls.querySelector('.play');
 let rwd = controls.querySelector('.rewind');
 let fwd = controls.querySelector('.forward');
-// let fullscreen = controls.querySelector('.fullscreen');
+let fullscreen = controls.querySelector('.fullscreen');
 
 
 let volumeIcon = controls.querySelector('.volume .icon');
@@ -42,12 +42,12 @@ play.addEventListener('click' , function() {
 })
 
 rwd.addEventListener('click' , function() {
-    media.currentTime = media.currentTime - 5;
+    media.currentTime = media.currentTime - 10;
 });
 
 
 fwd.addEventListener('click' , function() {
-    media.currentTime = media.currentTime + 5;
+    media.currentTime = media.currentTime + 10;
 });
 
 
@@ -63,6 +63,32 @@ volumeIcon.addEventListener('click' , function() {
 volumeProgressBarInput.addEventListener('input' , function() {
    media.volume = this.value / 100; 
    this.style = `background : linear-gradient(90deg, rgba(230,126,34,1) ${this.value}%, #e1e1e1 0%);`
+})
+
+
+fullscreen.addEventListener('click' , function() {
+    console.log(document.fullscreenElement)
+    if (!document.fullscreenElement) {
+        if(playerArea.requestFullscreen) {
+            playerArea.requestFullscreen();
+        } else if(playerArea.mozFullScreenElement) {
+            playerArea.mozFullScreenElement()
+        } else if(playerArea.msFullscreenElement) {
+            playerArea.msFullscreenElement()
+        } else if(playerArea.webkitFullscreenElement) {
+            playerArea.webkitFullscreenElement()
+        }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen(); 
+      } else if(document.mozCancelFullscreen) {
+        document.mozCancelFullscreen(); 
+      } else if(document.msCancelFullscreen) {
+        document.msCancelFullscreen(); 
+      } else if(document.webkitCancelFullscreen) {
+        document.webkitCancelFullscreen(); 
+      }
+    }
 })
 
 function togglePlayIcon() {
